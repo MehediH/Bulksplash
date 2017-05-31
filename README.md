b196d9a58e48e854200b1867b5b3a618ba8c96c7
hackerman <hackerman@Mehedis-MacBook-Pro.local>
2017-06-01 00:46:11 +0100

    Bulksplash

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-# Unsplash Bulk Downloader
-### A simple command line tool that lets you bulk download images from Unsplash
+# Bulksplash
+
+### A simple command line tool that lets you bulk download images from Unsplash, previously known as Unsplash Bulk Downloader
 
 ![screenshot](http://i.imgur.com/E0NXUQp.jpg)
 
-## Getting started
+## What's New
 
-To get started with Unsplash Bulk Downloader, simply clone the repo, install the modules with npm and run `node index`
+Bulksplash is now powered by the official Unsplash API. The new API gives us access to some new features, including:
+
+* You can now download the featured images from Unsplash
+* You can now download images with a specific search query
+* You can now choose the orientation of the images
+
+## Getting Started
+
+To get started with Bulksplash, simply clone the repo, install the modules with `npm  install` and run `node index`
 
 Here are some of the available options:
 
@@ -14,14 +23,15 @@ Usage: index [options]
 
   Options:
 
-    -h, --help        output usage information
-    -V, --version     output the version number
-    --amount [limit]  Amount of pictures to download (Default is 10)
-    --folder [name]   Name of the folder you want to save the images to (Default is "images")
-    --width [w]       Width of images (Default is 1200)
-    --height [h]      Height of images (Default is 800)
-    -g, --grayscale   Enable grayscale for images (disabled by default)
-    -b, --blur        Blur images (disabled by default)
+    -h, --help         output usage information
+    -V, --version      output the version number
+    --amount [limit]   Amount of pictures to download (Default is 10)
+    --folder [name]    Name of the folder you want to save the images to (Default is "images")
+    --width [w]        Width of images (Default is 1200)
+    --height [h]       Height of images (Default is 800)
+    --featured [f]     Download featured images only
+    --search [s]       Download images with a specific term
+    --orientation [o]  Image orientation (landscape, portrait, and squarish
 ````
 
 ---
--- index.js
+++ index.js
@@ -10,8 +10,9 @@ program
   .option('--folder [name]', 'Name of the folder you want to save the images to (Default is "images")')
   .option('--width [w]', 'Width of images (Default is 1200)')
   .option('--height [h]', 'Height of images (Default is 800)')
-  .option('-g, --grayscale', 'Enable grayscale for images (disabled by default)')
-  .option('-b, --blur', 'Blur images (disabled by default)')
+  .option('--featured [f]', 'Download featured images only')
+  .option('--search [s]', 'Download images with a specific term')
+  .option('--orientation [o]', 'Image orientation (landscape, portrait, and squarish')
   .parse(process.argv);
 
 if(!program.amount){
@@ -22,12 +23,8 @@ if(!program.folder){
     program.folder = "images"
 }
 
-if(!program.width){
-    program.width = 1200
-}
-
-if(!program.height){
-    program.height = 800
+if(!program.orientation){
+    program.orientation= "landscape"
 }
 
 console.log("Downloading " + program.amount + " random images :)")
@@ -36,31 +33,48 @@ var bar = new ProgressBar(':bar', { total: parseInt(program.amount) });
 
 function download(url, dest, dirname) {
     var dir = './' + dirname;
+
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir)
     }
 
     var file = fs.createWriteStream(dest);
     var request = https.get(url, function(response) {
-        response.pipe(file)
-        file.on('finish', function() {
-            bar.tick()
-        })
-    }).on('error', function(err) { 
-        fs.unlink(dest) 
+        response.pipe(file);
+        bar.tick();
     });
 };
 
-for (i = 0; i < program.amount; i++){
-    if(program.grayscale){
-        var url = "https://unsplash.it/g/" + program.width + "/" + program.height + "/?random"
-    } else{
-        var url = "https://unsplash.it/" + program.width + "/" + program.height + "/?random"
-    }
+var url = "https://api.unsplash.com/photos/random?count=" + program.amount + "&orientation=" + program.orientation + "&client_id=03d23cbab4fb050ab00383b955af3e9e5cfaf48141f8e8074f9068ece9689216"
 
-    if(program.blur){
-        var url = url + "&blur"
-    }
-
-    download(url, path.join(__dirname, "/" + program.folder + "/image-" + (i+1) + ".jpg"), program.folder)
+if(program.width){
+    var url = url + "&w=" + program.width;
 }
+
+if(program.height){
+    var url = url + "&h=" + program.height;
+}
+
+if(program.search){
+    var url = url + "&query=" + program.search;
+}
+
+request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        var body = JSON.parse(body);
+
+        
+        for (i in body){
+            if(program.width || program.height){
+                var img = body[i]["urls"].custom
+            } else{
+                var img = body[i]["urls"].raw
+            }
+
+            download(img, path.join(__dirname, "/" + program.folder + "/image-" + i + ".jpg"), program.folder)
+        }
+
+    } else {
+        console.log("Got an error: ", error)
+    }
+})
